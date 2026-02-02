1dac48a16cfd2434302231553b1ae226a7b17722
Tareef-debugger <tareeftoufique@gmail.com>
2026-02-02 15:26:31 +0600

    Add files via upload

---
+++ activityone.py
@@ -0,0 +1,9 @@
+print("What is the type of above given - \n 1) Relational \n 2)Non relational")
+answer = int(input("Enter your guess here........."))
+if answer == 2:
+    print("You guessed it right!!!")
+else:
+    print("Unfortunately your guess is wrong")
+
+
+print("\n Pleasse tel your mentor why you guessed this???")
